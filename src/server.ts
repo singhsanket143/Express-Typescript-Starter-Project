c2337955b156287a0c50123e3bfe90ac67598032
Sanket Singh <singhsanket143@gmail.com>
2025-04-16 23:11:33 +0530

    Added winston

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import { serverConfig } from './config';
 import v1Router from './routers/v1/index.router';
 import v2Router from './routers/v2/index.router';
 import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
-
+import logger from './config/logger.config';
+import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
 const app = express();
 
 app.use(express.json());
@@ -11,6 +12,8 @@ app.use(express.json());
 /**
  * Registering all the routers and their corresponding routes with out app server object.
  */
+
+app.use(attachCorrelationIdMiddleware);
 app.use('/api/v1', v1Router);
 app.use('/api/v2', v2Router); 
 
@@ -24,6 +27,6 @@ app.use(genericErrorHandler);
 
 
 app.listen(serverConfig.PORT, () => {
-    console.log(`Server is running on http://localhost:${serverConfig.PORT}`);
-    console.log(`Press Ctrl+C to stop the server.`);
+    logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
+    logger.info(`Press Ctrl+C to stop the server.`);
 });
